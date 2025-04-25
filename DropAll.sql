3f5a44554a262f97fb9d7953323ad3612215d8c1
Pedrovmc04 <pedrovmc16@gmail.com>
2025-04-25 10:40:27 -0300

    mudando a dropALL

--- DropAll.sql
+++ DropAll.sql
@@ -9,11 +9,12 @@ DROP TABLE IF EXISTS Acesso_Energia_Renovavel CASCADE;
 DROP TABLE IF EXISTS Acesso_Eletricidade CASCADE;
 DROP TABLE IF EXISTS Unidade_Geradora CASCADE;
 DROP TABLE IF EXISTS Usina CASCADE;
+DROP TABLE IF EXISTS Regiao CASCADE;
 
 -- Drop tables without dependencies last
-DROP TABLE IF EXISTS Pais CASCADE;
 DROP TABLE IF EXISTS Agente_Proprietario CASCADE;
-DROP TABLE IF EXISTS Regiao CASCADE;
+DROP TABLE IF EXISTS Estado CASCADE;
+DROP TABLE IF EXISTS Pais CASCADE;
 
 -- Confirmation message
 SELECT 'All tables have been dropped successfully.' as result;
